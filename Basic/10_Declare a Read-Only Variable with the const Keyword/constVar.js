3b882308a35d7861b6b04965f49fb7fbd03b1061
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-09-23 11:48:57 +0700

    add until 10 item

---
+++ Basic/10_Declare a Read-Only Variable with the const Keyword/constVar.js
@@ -0,0 +1,7 @@
+// const has all the awesome features that let has, with the added bonus that variables declared using const are read-only. They are a constant value, which means that once a variable is assigned with const, it cannot be reassigned
+
+// Change the code so that all variables are declared using let or const. Use let when you want the variable to change, and const when you want the variable to remain constant. Also, rename variables declared with const to conform to common practices. Do not change the strings assigned to the variables.
+const fCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(fCC, fact); // Change this line
---
+++ Basic/9_Explore Differences Between the var and let Keywords/varAndLet.js
@@ -0,0 +1,4 @@
+// One of the biggest problems with declaring variables with the var keyword is that you can easily overwrite variable declarations
+// Update the code so it only uses the let keyword.
+let catName = "Oliver"; //diganti tadinya var
+let catSound = "Meow!";
