d79a4e2ab8c6290b838036b175972eb3edfb225f
ariefhk <ariefrachmanhakim1001@gmail.com>
2022-09-23 10:13:58 +0700

    add multiline comment

--- Basic/1_Comment Your JavaScript Code/comment.js
+++ Basic/1_Comment Your JavaScript Code/comment.js
@@ -1,2 +1,9 @@
-// Komentari Code JS
-// Tips Ctrl + /
+// Komentari Code JS (CTRL + /)
+// 1. Single Line Comment
+// Ini contoh Single line comment
+
+// 2. Multi Line Comment
+/*
+Ini adalah contoh multiline comment
+bisa nampung banyak hal
+*/
